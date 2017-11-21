d72028d1254380f9b278cacc346cfd6c857a5be5
sandy1013 <santosh.kosuri@gmail.com>
2017-11-21 17:44:43 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Hi i am a React Application</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
